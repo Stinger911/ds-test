9e331cce4d120e60c79fc97bc633e8d16d070aac
Andrew Abramov <a.abramov@qiwi.com>
2020-03-19 17:14:04 +0300

    pwa release for develop/7e4d23c6

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.803717da2957b494c1b04ee6a89ef492.js"
+  "precache-manifest.c8bdc5014528477401921aabb18653cf.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "front"});
